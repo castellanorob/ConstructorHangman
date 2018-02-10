c51404b102fa6b9060a1d5487f7accf829f47357
Robert Castellano <castellanorob@gmail.com>
2018-02-10 13:57:34 -0600

    Updated letter.js

--- letter.js
+++ letter.js
@@ -1,13 +1,27 @@
 // Constructor that defines:
-// A string value to store the underlying character for the letter
 function Letter() {
+    // A string value to store the underlying character for the letter
+    this.char = "";
+    // A boolean value that stores whether that letter has been guessed yet
+    this.guessed = false;
 
-}
+    // A function that returns the underlying character if the letter has been guessed, 
+    // or a placeholder (like an underscore) if the letter has not been guessed
+    this.ifGuessed = function() {
+        if (this.guessed === true) {
+            return this.char;
+        } else {
+            return "_"
+        }
+    };
+    // A function that takes a character as an argument and checks it against the underlying character, 
+    // updating the stored boolean value to true if it was guessed correctly
+    this.guessChar = function() {
+        if (this.char === )
+    }
+};
 
-// A boolean value that stores whether that letter has been guessed yet
+module.exports = Letter
 
-// A function that returns the underlying character if the letter has been guessed, 
-// or a placeholder (like an underscore) if the letter has not been guessed
 
-// A function that takes a character as an argument and checks it against the underlying character, 
-// updating the stored boolean value to true if it was guessed correctly
+
