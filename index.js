d483a8dc86f3ac6463a0affbb5d18c33b7a8b733
Robert Castellano <castellanorob@gmail.com>
2018-02-14 18:30:09 -0600

    Updated word.js, index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Requiring word.js
+
+// Create a contructor that calls the functions from the constructor in word.js
+
+// Call the function to start the game
--- word.js
+++ word.js
@@ -6,9 +6,14 @@ var letter = require('./letter.js');
 function Word() {
     // An array of new Letter objects representing the letters of the underlying word
     this.letters = [];
+
+    var word = "Toyota";
+
     // A function that returns a string representing the word
     this.wordFunc = function() {
+        var wordArray = word.split();
         
+
     }
     // A function that takes a character as an argument and calls the guess function on each letter object
 }
